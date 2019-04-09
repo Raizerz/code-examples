1b2a5fbd2fa8d5e6bcbd6dc669b3323d01fb61ba
Igor Tsarenko <Raizerz@users.noreply.github.com>
2019-04-09 17:44:13 +0300

    Update get-bills-of-sale-data.js

--- middlewares/get-bills-of-sale-data.js
+++ middlewares/get-bills-of-sale-data.js
@@ -14,7 +14,7 @@ module.exports = async (ctx, next) => {
     getUser(ctx.apiServices, deal.get('purchase.userId'), jwt),
     getUser(ctx.apiServices, deal.get('sell.userId'), jwt),
     getVehicleByVin(ctx.apiServices, deal.get('source.data.vin'), deal.get('sell.orgId'), jwt),
-    getDealer(ctx.DMXServices, deal.get('sell.orgId'), jwt),
+    getDealer(ctx.apiServices, deal.get('sell.orgId'), jwt),
   ]).catch(error => ctx.throwLog(`Error found when populating data for deal with id: ${deal.get('id')}`, error));
 
   const billOfSaleData = {
